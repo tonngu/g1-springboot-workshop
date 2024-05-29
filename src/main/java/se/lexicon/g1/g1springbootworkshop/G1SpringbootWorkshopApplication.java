3b3203f027465103f512e20cb2132869860f9a8d
Tony Nguyen <tonynguyen7396@gmail.com>
2024-05-29 09:51:33 +0200

    Initial commit for part 1

---
+++ src/main/java/se/lexicon/g1/g1springbootworkshop/G1SpringbootWorkshopApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g1.g1springbootworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G1SpringbootWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G1SpringbootWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/g1/g1springbootworkshop/G1SpringbootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g1.g1springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G1SpringbootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
